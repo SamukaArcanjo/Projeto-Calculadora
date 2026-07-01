8b47a39e49798eace29b29edc286cc42cfc78847
SamukaArcanjo <samuelarcanjo57@gmail.com>
2026-07-01 18:59:50 -0300

    Completo a calculadora

--- script.js
+++ script.js
@@ -1,9 +1,55 @@
-let opcoes = document.querySelectorAll('.opcao')
-let result = document.getElementById('resultado')
+let display = document.getElementById('display')
+let numero = document.querySelectorAll('.numero')
+let limpar = document.getElementById('limpar')
+let operadores = document.querySelectorAll('.operador')
+let calcular = document.getElementById('calcular')
 
-opcoes.forEach(function(btn) {
-    btn.addEventListener('click', function() {
+let valorAtual = ''
+let primeiroNumero = null
+let operadorAtual = ''
 
-        result.textContent = `Clicou no ${btn.textContent}`
+numero.forEach(function(botao) {
+    botao.addEventListener('click', function() {
+        valorAtual += botao.textContent
+        display.textContent = valorAtual
     })
+})
+
+
+operadores.forEach(function(operador) {
+    operador.addEventListener('click', function() {
+        primeiroNumero = valorAtual
+        operadorAtual = operador.textContent
+        valorAtual = ''
+    })
+})
+
+calcular.addEventListener('click', function() {
+    const a = parseFloat(primeiroNumero)
+    const b = parseFloat(valorAtual)
+    let resultado
+
+    if (operadorAtual === '+') {
+        resultado = a + b
+    }
+    if (operadorAtual === 'x') {
+        resultado = a * b
+    }
+    if (operadorAtual === '-') {
+        resultado = a - b
+    }
+    if (operadorAtual === '/') {
+        resultado = a / b
+    }
+
+    display.textContent = resultado
+    valorAtual = resultado
+})
+
+limpar.addEventListener('click', function() {
+    display.textContent = ''
+    valorAtual = ''
+    operadorAtual = ''
+    primeiroNumero = null
+
 })
